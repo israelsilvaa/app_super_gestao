d961548a244c66310ef6999800a9a72cc5fafd5f
israel silva <Israel524.is@gmail.com>
2023-01-24 15:18:17 -0300

    Inicio da aplicação

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\ContatoController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Verbo Http.
+// get
+// put 
+// post
+// patch
+// delete 
+// options
+
+Route::get('/', [PrincipalController::class, 'index'])->name('site.index');
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobre-nos');
+Route::get('/contatos', [ContatoController::class, 'contatos'])->name('site.contatos');
+Route::get('/login', function(){return 'login';})->name('site.login');
+
+Route::prefix('/app')->group(function () {
+    Route::get('/clientes',function(){return 'clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function(){return 'fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
+});
