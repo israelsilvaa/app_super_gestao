cb4228d9e26364d674e55fccddbaca5f3764137e
israel silvaa <Israel524.is@gmail.com>
2023-02-01 14:26:52 -0300

    aula 56
    escapando a as chaves de impressão do blade.
    
    *obs: feita em conjunto com a aula 55

--- resources/views/site/curso/aulas/aula55.blade.php
+++ resources/views/site/curso/aulas/aula55.blade.php
@@ -9,13 +9,17 @@
     <p>Desvia o fluxo caso o array a ser vazio esteja vazio.</p>
     @isset($array)
         @forelse ($array as $indice => $objeto)
-            <p>Carro: {{ $objeto['nome'] }}</p>
+            
             <p>Nome: {{ $objeto['carro'] }}</p>
             <p>Dinheiro: {{ $objeto['dinheiro'] ?? 'Dado não preenchido' }}</p>
             <p>Telefone: {{ $objeto['ddd'] }} {{ $objeto['telefone'] }}</p>
             <hr>
         @empty
-            Não existem cadastros!
+            Não existem cadastros!<hr>
+            <h3>Aula - 56.</h3>
+            <h3 class="red">escapando os caracteres de impressão do blade'</h3> 
+            <p>utilizamos o @ antes das chaves, e assim elas já podem ser impressas</p>
+            <p>Carro: @{{ $objeto['nome'] }}</p>
     @endforelse
 @endisset
 
