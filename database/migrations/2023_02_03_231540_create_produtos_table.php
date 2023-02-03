809c1ad8b2816bc81f59895b1081ed4d2157fd90
israel silva <Israel524.is@gmail.com>
2023-02-03 20:30:42 -0300

    aula 84
    Migration - Adicionando chaves estrangeiras (Relacionamento um para um)

--- database/migrations/2023_02_03_231540_create_produtos_table.php
+++ database/migrations/2023_02_03_231540_create_produtos_table.php
@@ -18,8 +18,8 @@ class CreateProdutosTable extends Migration
             $table->string('nome', 100);
             $table->text('descricao')->nullable();
             $table->integer('peso')->nullable();
-            $table->float('preco_venda', 8,2)->default(0.01);
-            $table->integer('estoque_minimo')->default(1);
+            $table->float('preco_venda', 8, 2)->default(0.01);
+            $table->integer('estoque_monimo')->default(1);
             $table->integer('estoque_maximo')->default(1);
             $table->timestamps();
         });
