a881efd157a632a3d96c88be407c1735b4e3ad06
israel silva <Israel524.is@gmail.com>
2023-02-05 14:18:38 -0300

    aula 92 - Eloquent
    Ajustando o nome da tabela no Model para um correto ORM

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
-    //
+    // esse nome sobrepõem o nome automático que o ORM Eloquent usa p/ persitir no banco.
+    protected $table = "fornecedores";
 }
