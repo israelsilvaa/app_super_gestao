d9fc6e15be6494dd84b23e57b1581946891c68dc
israel silva <Israel524.is@gmail.com>
2023-02-05 14:33:06 -0300

    aula 93 - Eloquent
    -Inserindo registros com Create e Fillable

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -8,4 +8,14 @@ class Fornecedor extends Model
 {
     // esse nome sobrepõem o nome automático que o ORM Eloquent usa p/ persitir no banco.
     protected $table = "fornecedores";
+
+    /* Habilita o preenchimento de atrtibutos atravez do metodo ::CREATE 
+     Comando.
+     \App\Fornecedor::create([
+     'nome'=>'fornecedor ABC', 
+     'site'=>'abcfornecedor.com',
+     'uf'=>'SP',
+     'email'=>'abc@teste.com']);
+    */
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
