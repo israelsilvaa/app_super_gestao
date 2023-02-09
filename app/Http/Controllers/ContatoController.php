6684b422f57b3832b754afbb58691e0413e81ded
israel silvaa <Israel524.is@gmail.com>
2023-02-09 13:23:49 -0300

    aula 121
    Gravando os dados do formulário no banco de dados

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,14 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato(Request $request){
+    public function contato(Request $request)
+    {
+        
+        $contato = new SiteContato();
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+        $contato->save();
+        
+        /*
+        // Para utililxar metofo FILL, deves-ser configura-lo no model:
+        // protected $tfillable = ['nome', 'idade'];
+        $contato = new SiteContato();
+        $contato->fill($request->all());
+        $contato->save();
+        */
 
-        echo '<pre>';
-        dd($request->all());
-        echo '</pre>';
-        return view('site.contato', ['titulo' => 'Super Gestão - Contato']);
+        return view('site.contato', ['titulo' => 'Super Gestão - Contato'])->with('msg', 'Mensagem enviada com sucesso!');
     }
 }
