a1c1bf47746ddd0bb24e84d2ac8681515b232d2e
israel silva <Israel524.is@gmail.com>
2023-02-08 21:12:37 -0300

    aula 120
    entendendo o Request

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -6,10 +6,11 @@ use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato(){
+    public function contato(Request $request){
 
-        var_dump($_POST);
-
+        echo '<pre>';
+        dd($request->all());
+        echo '</pre>';
         return view('site.contato', ['titulo' => 'Super Gestão - Contato']);
     }
 }
