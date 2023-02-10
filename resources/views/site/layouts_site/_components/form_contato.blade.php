ee5cc3f1f7ed88b1fe1e679e8525ebd488dba056
israel silvaa <Israel524.is@gmail.com>
2023-02-10 15:44:34 -0300

    aula 133
     Customizando a apresentação dos erros de validação parte 2

--- resources/views/site/layouts_site/_components/form_contato.blade.php
+++ resources/views/site/layouts_site/_components/form_contato.blade.php
@@ -2,10 +2,14 @@
 <form action="{{ Route('site.contato') }}" method="post">
     @csrf
     <input name="nome" value="{{ old('nome') }}" type="text" placeholder="Nome" class="{{ $classe }}">
+    <span class="msg-erro"> {{ $errors->has('nome') ? $errors->first('nome') : '' }}</span>
     <br>
-    <input name="telefone" value="{{ old('telefone') }}" type="text" placeholder="Telefone" class="{{ $classe }}">
+    <input name="telefone" value="{{ old('telefone') }}" type="text" placeholder="Telefone"
+        class="{{ $classe }}">
+    <span class="msg-erro"> {{ $errors->has('telefone') ? $errors->first('telefone') : '' }}</span>
     <br>
     <input name="email" value="{{ old('email') }}" type="text" placeholder="E-mail" class="{{ $classe }}">
+    <span class="msg-erro"> {{ $errors->has('email') ? $errors->first('email') : '' }}</span>
     <br>
     <select name="motivo_contatos_id" class="{{ $classe }}">
 
@@ -17,22 +21,16 @@
         @endforeach
 
     </select>
+    <span class="msg-erro"> {{ $errors->has('motivo_contatos_id') ? $errors->first('motivo_contatos_id') : '' }}</span>
     <br>
     <textarea name="mensagem" class="{{ $classe }}" placeholder="Preencha aqui a sua mensagem">
 @if (old('mensagem') != '')
 {{ old('mensagem') }}
 @endif
 </textarea>
+    <span class="msg-erro"> {{ $errors->has('mensagem') ? $errors->first('mensagem') : '' }}</span>
     <br>
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
 
-@if ($errors->any())
-    <div style="position: absolute;top:0px; left:0px; width:100%; background: red">
-        <pre>
-            @foreach ($errors->all() as $erro)
-                {{ $erro }}<br>
-            @endforeach
-        </pre>
-    </div>
-@endif
+
