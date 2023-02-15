9a69b5922506b81f4456508acc0d3259c8972c48
israel silva <Israel524.is@gmail.com>
2023-02-15 20:56:24 -0300

    aula 167
    Implementando o cadastro de produtos parte 6 (edit)

--- resources/views/app/produto/index.blade.php
+++ resources/views/app/produto/index.blade.php
@@ -39,7 +39,7 @@
                                 <td>{{ $produto->unidade_id }}</td>
                                 <td><a href="{{route('produtos.show', ['produto'=>$produto->id])}}">Visualizar</a></td>
                                 <td><a href="#">Excluir</a></td>
-                                <td><a href="#">Editar</a></td>
+                                <td><a href="{{ route('produtos.edit', ['produto' =>$produto->id ]) }}">Editar</a></td>
                             </tr>
                         @endforeach
                     </tbody>
