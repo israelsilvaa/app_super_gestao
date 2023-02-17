0b4ef085e3d5dfe47495abbdd2be365bd2040d32
israel silva <Israel524.is@gmail.com>
2023-02-17 15:34:28 -0300

    aula 196
    Relacionamento N para N - Colunas pivô da tabela de relacionamento (Pivot)

--- resources/views/app/pedido_produto/create.blade.php
+++ resources/views/app/pedido_produto/create.blade.php
@@ -27,6 +27,7 @@
                         <tr>
                             <th>ID</th>
                             <th>Nome do produto</th>
+                            <th>Data de inclusão do item no pedido</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -34,10 +35,12 @@
                             <tr>
                                 <td>{{ $produto->id }}</td>
                                 <td>{{ $produto->nome }}</td>
+                                <td>{{ $produto->pivot->created_at->format('d/m/Y') }}</td>
                             </tr>
                         @endforeach
                     <tbody>
                 </table>
+
                 @component('app.pedido_produto._components.form_create', ['pedido' => $pedido, 'produtos' => $produtos])
                 @endcomponent
             </div>
